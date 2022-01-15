e0bf2d3cc3aec8e71fdfa8e9cdf9f14187190c33
Anurag Ravi <kumarayush1014@gmail.com>
2022-01-15 16:42:36 +0530

    upto account view&delete + posts view

--- faq/static/scripts/q_detail.js
+++ faq/static/scripts/q_detail.js
@@ -15,21 +15,21 @@ for (var i = 0; i < upvote_btns.length; i++) {
       success: function (data) {
         console.log(data);
         if (data == "added_upvote") {
-          $("#upvote-btn-" + ans_id).addClass("svg-accent");
+          $("#upvote-btn-" + ans_id).addClass("svg-accent-u");
           $("#upvote-value-" + ans_id).addClass("text-accent");
           var val = parseInt($("#upvote-value-" + ans_id).text());
           $("#upvote-value-" + ans_id).text((val + 1).toString());
         } else if (data == "removed_downvote_and_added_upvote") {
-          $("#upvote-btn-" + ans_id).addClass("svg-accent");
+          $("#upvote-btn-" + ans_id).addClass("svg-accent-u");
           $("#upvote-value-" + ans_id).addClass("text-accent");
           var val = parseInt($("#upvote-value-" + ans_id).text());
           $("#upvote-value-" + ans_id).text((val + 1).toString());
-          $("#downvote-btn-" + ans_id).removeClass("svg-accent");
+          $("#downvote-btn-" + ans_id).removeClass("svg-accent-d");
           $("#downvote-value-" + ans_id).removeClass("text-accent");
           var val = parseInt($("#downvote-value-" + ans_id).text());
           $("#downvote-value-" + ans_id).text((val - 1).toString());
         } else if (data == "removed_upvote") {
-          $("#upvote-btn-" + ans_id).removeClass("svg-accent");
+          $("#upvote-btn-" + ans_id).removeClass("svg-accent-u");
           $("#upvote-value-" + ans_id).removeClass("text-accent");
           var val = parseInt($("#upvote-value-" + ans_id).text());
           $("#upvote-value-" + ans_id).text((val - 1).toString());
@@ -54,21 +54,21 @@ for (var i = 0; i < downvote_btns.length; i++) {
       success: function (data) {
         console.log(data);
         if (data == "added_downvote") {
-          $("#downvote-btn-" + ans_id).addClass("svg-accent");
+          $("#downvote-btn-" + ans_id).addClass("svg-accent-d");
           $("#downvote-value-" + ans_id).addClass("text-accent");
           var val = parseInt($("#downvote-value-" + ans_id).text());
           $("#downvote-value-" + ans_id).text((val + 1).toString());
         } else if (data == "removed_upvote_and_added_downvote") {
-          $("#downvote-btn-" + ans_id).addClass("svg-accent");
+          $("#downvote-btn-" + ans_id).addClass("svg-accent-d");
           $("#downvote-value-" + ans_id).addClass("text-accent");
           var val = parseInt($("#downvote-value-" + ans_id).text());
           $("#downvote-value-" + ans_id).text((val + 1).toString());
-          $("#upvote-btn-" + ans_id).removeClass("svg-accent");
+          $("#upvote-btn-" + ans_id).removeClass("svg-accent-u");
           $("#upvote-value-" + ans_id).removeClass("text-accent");
           var val = parseInt($("#upvote-value-" + ans_id).text());
           $("#upvote-value-" + ans_id).text((val - 1).toString());
         } else if (data == "removed_downvote") {
-          $("#downvote-btn-" + ans_id).removeClass("svg-accent");
+          $("#downvote-btn-" + ans_id).removeClass("svg-accent-d");
           $("#downvote-value-" + ans_id).removeClass("text-accent");
           var val = parseInt($("#downvote-value-" + ans_id).text());
           $("#downvote-value-" + ans_id).text((val - 1).toString());
